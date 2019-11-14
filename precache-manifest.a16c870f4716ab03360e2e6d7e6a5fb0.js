03bc692e13207f391cb485f6763afa21027e7e9c
Polaris <tristynkcline@gmail.com>
2019-11-13 20:30:02 -0600

    Updates

---
+++ precache-manifest.a16c870f4716ab03360e2e6d7e6a5fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0abd21613f3d2668f4d83cf9a75dac",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e591b3a94b073bbb9679",
+    "url": "/clickyGame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ff7b00600d233d974e30",
+    "url": "/clickyGame/static/js/2.d20f16af.chunk.js"
+  },
+  {
+    "revision": "e591b3a94b073bbb9679",
+    "url": "/clickyGame/static/js/main.db4f33e8.chunk.js"
+  },
+  {
+    "revision": "258d4424f9878ecd4f99",
+    "url": "/clickyGame/static/js/runtime-main.bf29bac0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clickyGame/static/media/logo.25bf045c.svg"
+  }
+]);
